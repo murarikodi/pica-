e1f1ff2c525157cf5267dc262c822438095ac57f
murarikodi <151171318+murarikodi@users.noreply.github.com>
2024-11-15 00:16:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById('appInterfaceButton').addEventListener('click', function() {
+    window.location.href = 'https://www.canva.com/design/DAGWL51Q3RM/yKUIZIVn3_-pHRn5XCFMTg/edit?utm_content=DAGWL51Q3RM&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton';
+});
+
+document.getElementById('mobileInterfaceButton').addEventListener('click', function() {
+    window.location.href = 'https://www.canva.com/design/DAGWMjHc_B0/pebz7vA-aaAh1TF0T3DrAA/edit?utm_content=DAGWMjHc_B0&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton';
+});
